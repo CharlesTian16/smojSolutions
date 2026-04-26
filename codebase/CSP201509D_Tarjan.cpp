05a4f72f381b77c56f4a4b82d67042f932caf023
mRNA <1518926653@qq.com>
2026-04-26 20:05:45 +0800

    Tarjan(SCC)+链式前向星

--- codebase/CSP201509D_Tarjan.cpp
+++ codebase/CSP201509D_Tarjan.cpp
@@ -1,4 +1,4 @@
-/*Tarjan*/
+/*Tarjan算法求SCC+链式前向星(便于遍历出边)*/
 #include <cstdio>
 #include <vector>
 #define N 10005
@@ -14,9 +14,9 @@ bool instack[N];
 int t,dfn[N],low[N];
 vector<int> st;
 void add(int a,int b){
-    ver[++cnt] = b;
-    nxt[cnt] = head[a];
-    head[a] = cnt;
+    ver[++cnt] = b;      // cnt号边指向b
+    nxt[cnt] = head[a];  // cnt号边下一条边为head[a]
+    head[a] = cnt;       // head[a]表示从a出发的第一条边，第cnt号 
 }
 
 void tarjan(int u){
